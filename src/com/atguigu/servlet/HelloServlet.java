ad97b9755effff64b22296b7726c2a9ed672dc72
tom_plb <goodMorning_glb@atguigu.com>
2019-09-19 20:58:03 +0800

    59、servlet3.0-异步请求

--- src/com/atguigu/servlet/HelloServlet.java
+++ src/com/atguigu/servlet/HelloServlet.java
@@ -10,6 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException{
+		System.out.println(Thread.currentThread()+"start...");
+		try {
+			sayHello();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		resp.getWriter().write("hello...");
+		System.out.println(Thread.currentThread()+"end...");
+	}
+	
+	public void sayHello() throws Exception{
+		System.out.println(Thread.currentThread()+"processing...");
+		Thread.sleep(3000);
 	}
 }
